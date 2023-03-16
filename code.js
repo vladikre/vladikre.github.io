23ffc7069d614028b2c8e31bbbdfccfb2156cbda
vladikre <123611492+vladikre@users.noreply.github.com>
2023-03-16 00:36:50 -0400

    Add files via upload

---
+++ code.js
@@ -0,0 +1,13 @@
+function loginBttn() {
+   var user =document.getElementById("username").value;
+	var pass =document.getElementById("username").value;
+	
+	var userT = "vlad";
+	var passT = "cool";
+	
+	if (user == userT) {
+		if (pass == passT) {
+			window.location.href = "home/index.html";
+		}
+	}
+}
